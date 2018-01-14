e4134400835dab3626c6483aa72669ad17c0ba80
Olha Popovych <bartimeusurucskiy@gmail.com>
2018-01-14 23:30:43 +0200

    init config

---
+++ src/components/Autocomplete.vue
@@ -0,0 +1,139 @@
+<template>
+  <div>
+    <input :placeholder="placeholder" class="autocomplete" v-model="typed" @blur="verificaBlur($event)">
+    <ul class="autocompleteList" v-if="showResult">
+      <li v-if="noneFind" id="autocompleteNoResult">{{ noresults }}</li>
+      <li class="autocompleteItemsList" v-for="result in results" @click="select(result)">{{ result }}</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Autocomplete',
+  props: {
+    value: {
+      default: null
+    },
+    data: {
+      type: Array,
+      required: true
+    },
+    minlength: {
+      type: Number,
+      default: 3
+    },
+    noresults: {
+      type: String,
+      default: 'No matching results'
+    },
+    placeholder: {
+      type: String,
+      default: null
+    }
+  },
+  data () {
+    return {
+      typed: '',
+      showResult: false,
+      noneFind: false,
+      results: ''
+    }
+  },
+  watch: {
+    typed () {
+      if (this.typed.length >= this.minlength) {
+        let verifier = this.typed
+        let verify = this.data.filter(function (elem) {
+          return elem === verifier
+        })
+        if (verify.length === 0) {
+          this.filter()
+        }
+      }
+      if (this.typed.length < this.minlength) {
+        this.showResult = false
+        this.results = ''
+      }
+    }
+  },
+  methods: {
+    filter () {
+      const map = this.data.map(x => x.title);
+      let reg = new RegExp(this.typed.split('').join('\\w*').replace(/\W/, ''), 'i')
+      let result = map.filter(function (elem) {
+        if (elem.match(reg)) {
+          return elem
+        }
+      })
+      this.results = result
+      this.showResult = true
+      if (result.length === 0) {
+        this.noneFind = true
+      }
+    },
+    select (result) {
+      this.typed = result
+      this.$emit('input', this.typed)
+      this.showResult = false
+    },
+    verificaBlur (event) {
+      let self = this
+      setTimeout(function () {
+        let verificador = self.typed
+        let verifica = self.data.filter(function (elem) {
+          return elem === verificador
+        })
+        if (verifica.length === 0) {
+          self.typed = ''
+          self.$emit('input', self.typed)
+        }
+      }, 260)
+    }
+  }
+}
+</script>
+
+<style>
+  .autocomplete {
+    box-sizing: border-box;
+    display: block;
+    width: 100%;
+    padding: .375rem .75rem;
+    font-size: 1rem;
+    line-height: 1.5;
+    color: #495057;
+    background-color: #fff;
+    background-image: none;
+    background-clip: padding-box;
+    border: 1px solid #ced4da;
+    border-radius: .25rem;
+    transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+  }
+  .autocompleteList {
+    z-index: 6;
+    background-color: white;
+    box-sizing: border-box;
+    width: 100%;
+    border-left: 1px solid #ced4da;
+    border-right: 1px solid #ced4da;
+    border-bottom: 1px solid #ced4da;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    font-size: 14px;
+    font-family: Helvetica, sans-serif;
+    padding: 12px;
+  }
+  .autocompleteItemsList {
+    margin: 0;
+    padding: 5px;
+    cursor: default;
+  }
+  .autocompleteItemsList:hover {
+    background-color: #eef4ff;
+    font-weight: bold;
+  }
+  #autocompleteNoResult {
+    color: #bbbbbb;
+  }
+</style>
---
+++ src/components/Basic.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <autocomplete :data="myOptions" v-model="selected"></autocomplete>
+    <p v-for="i in myOptions">{{i.title}}</p>
+  </div>
+</template>
+<script>
+import Autocomplete from './Autocomplete'
+export default {
+  name: 'Basic',
+  components: {
+    Autocomplete
+  },
+  data () {
+    return {
+      myOptions: [
+        {
+          title: 'First Scene',
+          description: 'lorem ipsum dolor amet.',
+          thumbnail: 'http://lorempicsum.com/nemo/200/200/1'
+        }, {
+          title: 'Second Scene',
+          description: 'lorem ipsum dolor amet.',
+          thumbnail: 'http://lorempicsum.com/nemo/200/200/2'
+        }, {
+          title: 'Third Scene',
+          description: 'lorem ipsum dolor amet.',
+          thumbnail: 'http://lorempicsum.com/nemo/200/200/3'
+        }, {
+          title: 'Fourth Scene',
+          description: 'lorem ipsum dolor amet.',
+          thumbnail: 'http://lorempicsum.com/nemo/200/200/4'
+        }
+      ],
+      selected: ''
+    }
+  }
+}
+</script>
